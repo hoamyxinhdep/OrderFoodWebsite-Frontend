2621491bb7c5f8df8d0e61adf29c8b5cf07ba04b
hoamyxinhdep <103980605+hoamyxinhdep@users.noreply.github.com>
2024-05-31 14:35:58 +0700

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+
+import { Link } from 'react-router-dom'
+import MobileNav from './MobileNav'
+import MainNav from './MainNav'
+
+export default function Header() {
+  
+    return (
+        <div className="border-b-2 border-b-orange-500 py-6">
+          <div className="mx-auto flex justify-between ml-7">
+            <Link
+              to="/"
+              className="text-3xl font-bold tracking-tight text-orange-500 "
+            >
+              OrderFood
+            </Link>
+            <div className="md:hidden">
+              <MobileNav />
+            </div>
+            <div className="hidden md:block mr-10">
+              <MainNav />
+            </div>
+          </div>
+        </div>
+      );
+  
+}
